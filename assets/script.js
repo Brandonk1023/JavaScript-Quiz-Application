0b54cf426948a1101448b29891c4fe59d5b426eb
Brandonk1023 <brandonk1023@gmail.com>
2023-02-20 09:49:07 -0500

    added question array

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+let questions = [
+    {
+        question: "The = sign is what type of operator?",
+        choices: [
+            "Boolean",
+            "Assignment",
+            "String",
+            "Interger"
+        ]
+    },
+
+    {
+        question: "What HTML tag is used to link JavaScript to the HTML?",
+        options: [
+            "Script",
+            "Link",
+            "Div",
+            "Body"
+        ]
+    },
+
+    {
+        question: "Arrays are always indexed with which beginning number?",
+        options: [
+            "0",
+            "1",
+            "2",
+            "3"
+        ]
+    },
+    
+    {
+        question: "A useful tool for printing information to the console, and for debugging is?",
+        options: [
+            "print",
+            "Hello.world",
+            "console.log",
+            "tell.me"
+
+        ]
+    }
+
+]
